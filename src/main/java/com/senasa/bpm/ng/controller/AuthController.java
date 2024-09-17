5f4f21fb5c35e1494c51fa0d1b75b38912a4889e
Gino Alejandro <ginoalejandro2304@gmail.com>
2024-09-17 12:40:51 -0500

    crea authController

--- src/main/java/com/senasa/bpm/ng/controller/AuthController.java
+++ src/main/java/com/senasa/bpm/ng/controller/AuthController.java
@@ -1,47 +1,5 @@
-/*package com.senasa.bpm.ng.controller;
+package com.senasa.bpm.ng.controller;
 
-import com.senasa.bpm.ng.model.Producto;
-import com.senasa.bpm.ng.model.request.AuthRequest;
-import com.senasa.bpm.ng.model.response.ApiResponse;
-import com.senasa.bpm.ng.security.JwtUtil;
-import com.senasa.bpm.ng.service.UserServiceImpl;
-import com.senasa.bpm.ng.utility.ConstantUtil;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-    private final UserServiceImpl userService;
-
-    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserServiceImpl userService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-        this.userService = userService;
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@RequestBody AuthRequest authRequest) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
-        );
-
-        final UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
-
-        return ResponseEntity.ok(
-                ApiResponse.<String>builder()
-                        .code(ConstantUtil.OK_CODE)
-                        .message(ConstantUtil.OK_MESSAGE)
-                        .data(jwt)
-                        .build());
-    }
-}*/
+}
